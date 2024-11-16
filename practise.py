25cf9c8645129345f0b283e346cbc55db117a250
GURUPRASAD74167 <guruabhiram79@gmail.com>
2024-11-16 16:53:10 +0530

    day-2 python

---
+++ practise.py
@@ -0,0 +1,107 @@
+
+'''
+comprction op
+
+
+==
+
+!=
+
+<
+
+>
+
+
+
+
+
+'''
+# a = 'apple'
+
+# b = 'banna'
+
+# c = 'yellow'
+
+# if a == 'apple':
+#    print('one')
+# elif b == 'banna':
+#    print('two')
+# elif c == 'yellow':
+#    print('three')
+# else:
+#    print('halo this is three')
+
+
+'''
+
+{logical op}
+or   operater
+
+and   operater
+'''
+
+# x = 32;
+
+# y = 93;
+
+# z = 73;
+
+# if x < y and z > y:
+#    print('this is correct');
+# else:
+#    print('where are you')
+
+
+
+'''
+loop's
+
+for loop
+
+while loop
+
+'''
+
+
+# for i in range (0,10,2):
+#    print(i)
+
+
+
+# a = 10;
+
+# while a > 1:
+#    print("guru")
+#    a+=1
+
+
+
+# password = " ";
+
+# while len(password) < 6:
+#    password = input('enter your facebook password')
+
+#    print('successfully completed')
+
+
+
+'''
+conditional statement
+'''
+
+# g = 10;
+
+# if g > 3:
+#    print('samsung')
+# elif g > 4:
+#    print('apple 12')
+# else:
+#    print('this is false')
+
+
+
+
+
+
+
+
